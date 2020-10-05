fa8ea4604dff13ab41425add4275fcf1ea9abe8c
Matthew Adams <matthew.adams@northscaler.com>
2020-10-05 14:08:58 -0500

    use string symbol

--- src/main/enums/Enumeration.js
+++ src/main/enums/Enumeration.js
@@ -9,12 +9,13 @@ const UnknownEnumValueError = require('../errors/UnknownEnumValueError')
 const EnumClassNotInstantiableError = require('../errors/EnumClassNotInstantiableError')
 const { toUpperSnake } = require('@northscaler/string-support')
 const copyProperties = require('./copyProperties')
+const symbol = 'Enumeration@northscaler/enum-support'
 
 class Enumeration {
-  static _symbol = Symbol('Enumeration')
+  static _$ = symbol
 
   static isEnumerationClass (it) {
-    return Enumeration._symbol === it?._symbol
+    return Enumeration._$ === it._$
   }
 
   static isEnumerationInstance (it) {
